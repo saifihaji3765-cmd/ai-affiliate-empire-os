4816c14c072f548c212882d2540fe7646ad4ddbf
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-08 13:48:03 +0530

    Update supabase.js

--- server/database/supabase.js
+++ server/database/supabase.js
@@ -1,8 +1,3 @@
-"dependencies": {
-  "@supabase/supabase-js": "^2.45.4",
-  "bcryptjs": "^2.4.3",
-  "cors": "^2.8.5"
-}
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
